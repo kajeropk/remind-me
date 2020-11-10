e6256a7efae80f1ee1741118f85feb72080a574b
Kaique Munhoz <munhoz13km@gmail.com>
2020-11-09 21:50:31 -0300

    feat: Add eslint

--- src/ui/components/taskCard/index.js
+++ src/ui/components/taskCard/index.js
@@ -1,51 +1,50 @@
 import React from 'react';
-import {
-    Alert,
-} from 'react-native';
+import { Alert } from 'react-native';
+
+import close from '../../assets/images/close.png';
 
 import {
-     Card,CardContent,CardPriorityContent,CardPriority,CardCloseButton,CardCloseButtonIcon,CardTitleContent,CardTitle,CardDateContent, 
-     CardDate,CardStatusContent,CardStatus
-  } from './styles'
+  Card, CardContent, CardPriorityContent, CardPriority,
+  CardCloseButton, CardCloseButtonIcon, CardTitleContent, CardTitle,
+  CardDateContent, CardDate, CardStatusContent, CardStatus,
+} from './styles';
 
 const TaskCard = (task, key) => {
+  const alertMessage = () => {
+    Alert.alert('It is not available yet', 'Developers are working');
+  };
 
-    const alertMessage = () => {
-        Alert.alert('It is not available yet','Developers are working')
-    }
-
-    return(
-        <>
-        <Card key={key} status={task.status} onPress={() => alertMessage()}>
-            <CardContent>
-                <CardPriorityContent>
-                    <CardPriority>
-                        {'P' + task.priority}
-                    </CardPriority>
-                    <CardCloseButton onPress={() => alertMessage()}>
-                        <CardCloseButtonIcon source={require('../../assets/images/close.png')}>
-                        </CardCloseButtonIcon>    
-                    </CardCloseButton>
-                </CardPriorityContent>
-                <CardTitleContent>
-                    <CardTitle>
-                        {task.title}
-                    </CardTitle>
-                </CardTitleContent>
-                <CardDateContent>
-                    <CardDate>
-                        {task.date}
-                    </CardDate>
-                </CardDateContent>
-                <CardStatusContent >
-                    <CardStatus status={task.status}>
-                        {task.status}
-                    </CardStatus>
-                </CardStatusContent>
-            </CardContent>
-        </Card>
-        </>
-    )
-}
+  return (
+    <>
+      <Card key={key} status={task.status} onPress={() => alertMessage()}>
+        <CardContent>
+          <CardPriorityContent>
+            <CardPriority>
+              {`P${task.priority}`}
+            </CardPriority>
+            <CardCloseButton onPress={() => alertMessage()}>
+              <CardCloseButtonIcon source={close} />
+            </CardCloseButton>
+          </CardPriorityContent>
+          <CardTitleContent>
+            <CardTitle>
+              {task.title}
+            </CardTitle>
+          </CardTitleContent>
+          <CardDateContent>
+            <CardDate>
+              {task.date}
+            </CardDate>
+          </CardDateContent>
+          <CardStatusContent>
+            <CardStatus status={task.status}>
+              {task.status}
+            </CardStatus>
+          </CardStatusContent>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
 
 export default TaskCard;
