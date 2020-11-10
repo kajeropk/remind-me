d3d6d34def0ef0a1394ad9c0b9e4f5430e3c67ed
Kaique Munhoz <munhoz13km@gmail.com>
2020-11-09 21:56:20 -0300

    feat: Add prettier

--- src/ui/components/taskCard/index.js
+++ src/ui/components/taskCard/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Alert } from 'react-native';
+import {Alert} from 'react-native';
 
 import close from '../../assets/images/close.png';
 
 import {
-  Card, CardContent, CardPriorityContent, CardPriority,
-  CardCloseButton, CardCloseButtonIcon, CardTitleContent, CardTitle,
-  CardDateContent, CardDate, CardStatusContent, CardStatus,
+  Card,
+  CardContent,
+  CardPriorityContent,
+  CardPriority,
+  CardCloseButton,
+  CardCloseButtonIcon,
+  CardTitleContent,
+  CardTitle,
+  CardDateContent,
+  CardDate,
+  CardStatusContent,
+  CardStatus,
 } from './styles';
 
 const TaskCard = (task, key) => {
@@ -19,27 +28,19 @@ const TaskCard = (task, key) => {
       <Card key={key} status={task.status} onPress={() => alertMessage()}>
         <CardContent>
           <CardPriorityContent>
-            <CardPriority>
-              {`P${task.priority}`}
-            </CardPriority>
+            <CardPriority>{`P${task.priority}`}</CardPriority>
             <CardCloseButton onPress={() => alertMessage()}>
               <CardCloseButtonIcon source={close} />
             </CardCloseButton>
           </CardPriorityContent>
           <CardTitleContent>
-            <CardTitle>
-              {task.title}
-            </CardTitle>
+            <CardTitle>{task.title}</CardTitle>
           </CardTitleContent>
           <CardDateContent>
-            <CardDate>
-              {task.date}
-            </CardDate>
+            <CardDate>{task.date}</CardDate>
           </CardDateContent>
           <CardStatusContent>
-            <CardStatus status={task.status}>
-              {task.status}
-            </CardStatus>
+            <CardStatus status={task.status}>{task.status}</CardStatus>
           </CardStatusContent>
         </CardContent>
       </Card>
